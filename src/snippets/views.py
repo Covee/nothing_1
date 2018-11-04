a8702d8f55d2740ca9c83539da2c55fd5b18054d
Covee <uniqtop91@gmail.com>
2018-11-04 23:45:26 +0900

    view파일에 코드조각들을 GET, POST 할 function 정의

--- src/snippets/views.py
+++ src/snippets/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
 
-# Create your views here.
+
+class JSONResponse(HttpResponse):
+	"""
+	콘텐츠를 JSON으로 변환한 후 HttpResponse 형태로 반환합니다.
+	"""
+	def __init__(self, data, **kwargs):
+		content = JSONRenderer().render(data)
+		kwargs['content_type'] = 'application/json'
+		super(JSONResponse, self).__init__(content, **kwargs)
+
+
+@csrf_exempt 		# 이 데코레이터는 인증되지 않은 사용자도 이 뷰에 POST 할 수 있도록 함
+def snippet_list(request):  
+	"""
+	코드 조각을 모두 보여주거나 새 코드 조각을 만듭니다.
+	"""
+	if request.method == 'GET':
+		snippets = Snippet.objects.all()
+		serializer = SnippetSerializer(snippets, many=True)
+		return JSONResponse(serializer.data)
+
+	elif request.method == 'POST':
+		data = JSONParser().parse(request)
+		serializer = SnippetSerializer(data=data)
+		if serializer.is_valid():
+			serializer.save()
+			return JSONResponse(serializer.data, status=201)
+		return JSONResponse(serializer.errors, status=400)
