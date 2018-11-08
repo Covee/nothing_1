3da7cb6e1910af9235f71f33e3aeca5461edd913
Covee <uniqtop91@gmail.com>
2018-11-08 20:09:38 +0900

    mixin과 연결된 generic view 활용해서 더 간결하게 코드 작성

--- src/snippets/views.py
+++ src/snippets/views.py
@@ -1,39 +1,56 @@
-### Mixins: REST Framework에서 만들어둔 보편적인 기능들(CRUD같은). CBV일때 손쉽게 가져와서 쓸 수 있음.
+### Mixin과 연결된 Generic View 사용
 
 from snippets.models import Snippet  
 from snippets.serializers import SnippetSerializer  
-from rest_framework import mixins  
 from rest_framework import generics
 
 
-class SnippetList(mixins.ListModelMixin, 
-				  mixins.CreateModelMixin, 
-				  generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):  
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
 
-	def get(self, request, *args, **kwargs):
-		return self.list(request, *args, **kwargs)
 
-	def post(self, request, *args, **kwargs):
-		return self.create(request, *args, **kwargs)
-
-
-class SnippetDetail(mixins.RetrieveModelMixin, 
-					mixins.UpdateModelMixin, 
-					mixins.DestroyModelMixin, 
-					generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):  
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
 
-	def get(self, request, *args, **kwargs):
-		return self.retrieve(request, *args, **kwargs)
 
-	def put(self, request, *args, **kwargs):
-		return self.update(request, *args, **kwargs)
+### Mixins: REST Framework에서 만들어둔 보편적인 기능들(CRUD같은). CBV일때 손쉽게 가져와서 쓸 수 있음.
 
-	def delete(self, request, *args, **kwargs):
-		return self.destroy(request, *args, **kwargs)
+# from snippets.models import Snippet  
+# from snippets.serializers import SnippetSerializer  
+# from rest_framework import mixins  
+# from rest_framework import generics
+
+
+# class SnippetList(mixins.ListModelMixin, 
+# 				  mixins.CreateModelMixin, 
+# 				  generics.GenericAPIView):
+# 	queryset = Snippet.objects.all()
+# 	serializer_class = SnippetSerializer
+
+# 	def get(self, request, *args, **kwargs):
+# 		return self.list(request, *args, **kwargs)
+
+# 	def post(self, request, *args, **kwargs):
+# 		return self.create(request, *args, **kwargs)
+
+
+# class SnippetDetail(mixins.RetrieveModelMixin, 
+# 					mixins.UpdateModelMixin, 
+# 					mixins.DestroyModelMixin, 
+# 					generics.GenericAPIView):
+# 	queryset = Snippet.objects.all()
+# 	serializer_class = SnippetSerializer
+
+# 	def get(self, request, *args, **kwargs):
+# 		return self.retrieve(request, *args, **kwargs)
+
+# 	def put(self, request, *args, **kwargs):
+# 		return self.update(request, *args, **kwargs)
+
+# 	def delete(self, request, *args, **kwargs):
+# 		return self.destroy(request, *args, **kwargs)
 
 
 
