8d66280c170c96553f246290140bc3e547ac9799
Covee <uniqtop91@gmail.com>
2018-11-10 22:46:31 +0900

    instance를 저장할 수 있는 기능 추가

--- src/snippets/views.py
+++ src/snippets/views.py
@@ -9,6 +9,9 @@ class SnippetList(generics.ListCreateAPIView):
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
 
+	def perform_create(self, serializer):  
+		serializer.save(owner=self.request.user)
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):  
 	queryset = Snippet.objects.all()
@@ -26,7 +29,7 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):  
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
-	
+
 
 ### Mixins: REST Framework에서 만들어둔 보편적인 기능들(CRUD같은). CBV일때 손쉽게 가져와서 쓸 수 있음.
 
