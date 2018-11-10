708a0f543b92c7f9fe77f1871559287351599b42
Covee <uniqtop91@gmail.com>
2018-11-11 07:52:43 +0900

    permission 추가(허가받지 않은 접근에는 ReadOnly)

--- src/snippets/views.py
+++ src/snippets/views.py
@@ -2,12 +2,14 @@
 
 from snippets.models import Snippet  
 from snippets.serializers import SnippetSerializer  
-from rest_framework import generics
+from rest_framework import generics, permissions
+
 
 
 class SnippetList(generics.ListCreateAPIView):  
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
+	permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 	def perform_create(self, serializer):  
 		serializer.save(owner=self.request.user)
@@ -16,6 +18,7 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):  
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
+	permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
 from django.contrib.auth.models import User
