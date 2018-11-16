980bc2bc3cd67aa2fe7c8892e0be23198e0e8f30
Covee <uniqtop91@gmail.com>
2018-11-16 16:42:23 +0900

    hyperlink로 API연결

--- src/snippets/serializers.py
+++ src/snippets/serializers.py
@@ -5,12 +5,31 @@ from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 from django.contrib.auth.models import User
 
 
-class UserSerializer(serializers.ModelSerializer):  
-	snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):  
+	owner = serializers.ReadOnlyField(source='owner.username')
+	highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
+	class Meta:
+		model = Snippet
+		fields = ('url', 'highlight', 'owner',
+					'title', 'code', 'linenos', 'language', 'style')
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):  
+	snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
 	class Meta:
 		model = User
-		fields = ('id', 'username', 'snippets')
+		fields = ('url', 'username', 'snippets')
+
+
+
+# class UserSerializer(serializers.ModelSerializer):  
+# 	snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+# 	class Meta:
+# 		model = User
+# 		fields = ('id', 'username', 'snippets')
  
 ''' ModelSerializer를 안쓰면 이렇게 길게 직접 설정해주면 된다. '''
 
@@ -36,11 +55,11 @@ class UserSerializer(serializers.ModelSerializer):
 # 		return instance
 
 
-class SnippetSerializer(serializers.ModelSerializer):
-	owner = serializers.ReadOnlyField(source='owner.username')
+# class SnippetSerializer(serializers.ModelSerializer):
+# 	owner = serializers.ReadOnlyField(source='owner.username')
 
-	class Meta:
-		model = Snippet
-		fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+# 	class Meta:
+# 		model = Snippet
+		# fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
 
 
